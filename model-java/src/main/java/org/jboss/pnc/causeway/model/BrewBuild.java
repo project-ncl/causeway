1f72c3121d1ff3c36e74fc343cd59e0da8707da6
Petr Sakař <psakar@redhat.com>
2016-02-16 14:35:49 +0100

    fix mapping to json for imported build

--- model-java/src/main/java/org/jboss/pnc/causeway/model/BrewBuild.java
+++ model-java/src/main/java/org/jboss/pnc/causeway/model/BrewBuild.java
@@ -1,8 +1,16 @@
 package org.jboss.pnc.causeway.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class BrewBuild extends BrewNVR {
 
     private final Long id;
+
+    @JsonCreator
+    public BrewBuild(@JsonProperty("id")Long id, @JsonProperty("name")String name, @JsonProperty("version")String version, @JsonProperty("release")String release) {
+        this(id, new BrewNVR(name, version, release));
+    }
 
     public BrewBuild(Long id, BrewNVR nvr) {
         super(nvr.getName(), nvr.getRelease(), nvr.getVersion());
--- model-java/src/main/java/org/jboss/pnc/causeway/model/ImportedBuild.java
+++ model-java/src/main/java/org/jboss/pnc/causeway/model/ImportedBuild.java
@@ -1,12 +1,16 @@
 package org.jboss.pnc.causeway.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class ImportedBuild
 {
     private final Long buildId;
 
     private final BrewBuild brewBuild;
 
-    public ImportedBuild(Long buildId, BrewBuild brewBuild) {
+    @JsonCreator
+    public ImportedBuild(@JsonProperty("buildId")Long buildId, @JsonProperty("brewBuild")BrewBuild brewBuild) {
         this.buildId = buildId;
         this.brewBuild = brewBuild;
     }
