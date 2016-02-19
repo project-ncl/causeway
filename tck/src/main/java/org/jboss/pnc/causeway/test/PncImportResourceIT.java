541032f23e83abb3dad1361171ec45b7b2a1731f
Petr Sakař <psakar@redhat.com>
2016-02-20 00:20:32 +0100

    rename *IntegrationTest to *IT

--- tck/src/main/java/org/jboss/pnc/causeway/test/PncImportResourceIT.java
+++ tck/src/main/java/org/jboss/pnc/causeway/test/PncImportResourceIT.java
@@ -1,24 +1,20 @@
 package org.jboss.pnc.causeway.test;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpGet;
 import org.jboss.pnc.causeway.test.util.HttpCommandResult;
 import org.junit.Test;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.Date;
-
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertThat;
 
 /**
  * Created by jdcasey on 2/11/16.
  */
-public class PncImportResourceTest
+public class PncImportResourceIT
     extends AbstractTest
 {
     @Test
@@ -32,6 +28,9 @@ public class PncImportResourceTest
             {
                 String url = formatUrl( "/import/test", var );
                 HttpGet request = new HttpGet( url );
+                request.setHeader("Content-Type", "application/json");
+                request.setHeader("Accept", "application/json");
+                request.setHeader("RESTEASY_CHOSEN_ACCEPT", "*");
 
                 HttpResponse response = client.execute( request );
                 assertThat( response.getStatusLine().getStatusCode(), equalTo( 200 ) );
