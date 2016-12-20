52efdc18be991bfd70a0d1a66812e563341c80a4
Honza Brázdil <jbrazdil@redhat.com>
2016-12-20 17:49:29 +0100

    Update license headers and fix @author javadoc

--- rest/src/main/java/org/jboss/pnc/causeway/rest/BrewPushMilestoneResponse.java
+++ rest/src/main/java/org/jboss/pnc/causeway/rest/BrewPushMilestoneResponse.java
@@ -1,11 +1,11 @@
-/*
- * Copyright 2016 Honza Brázdil <jbrazdil@redhat.com>.
+/**
+ * Copyright (C) 2015 Red Hat, Inc. (jbrazdil@redhat.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *         http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -20,7 +20,7 @@ import lombok.NonNull;
 
 /**
  *
- * @author Honza Brázdil <jbrazdil@redhat.com>
+ * @author Honza Brázdil &lt;jbrazdil@redhat.com&gt;
  */
 @Data
 public class BrewPushMilestoneResponse {
