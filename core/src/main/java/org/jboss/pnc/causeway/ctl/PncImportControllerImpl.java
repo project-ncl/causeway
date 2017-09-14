eb610421d9bea635a902ff2b8213277495ed00de
Honza Brázdil <jbrazdil@redhat.com>
2017-09-14 12:25:17 +0200

    Remove transactions from controller

--- core/src/main/java/org/jboss/pnc/causeway/ctl/PncImportControllerImpl.java
+++ core/src/main/java/org/jboss/pnc/causeway/ctl/PncImportControllerImpl.java
@@ -35,6 +35,8 @@ import org.jboss.pnc.rest.restmodel.causeway.ReleaseStatus;
 
 import javax.ejb.Asynchronous;
 import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
 import javax.inject.Inject;
 
 import java.util.ArrayList;
@@ -46,6 +48,7 @@ import java.util.logging.Logger;
 import com.redhat.red.build.koji.model.json.KojiImport;
 
 @Stateless
+@TransactionAttribute(TransactionAttributeType.NOT_SUPPORTED)
 public class PncImportControllerImpl implements PncImportController {
 
     private final PncClient pncClient;
