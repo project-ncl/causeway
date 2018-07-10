a649910a4498388e4fa3c20ad1a2f41abc80251e
Honza Brázdil <jbrazdil@redhat.com>
2018-07-10 18:02:28 +0200

    Improve logging

--- core/src/main/java/org/jboss/pnc/causeway/ctl/ImportControllerImpl.java
+++ core/src/main/java/org/jboss/pnc/causeway/ctl/ImportControllerImpl.java
@@ -68,7 +68,7 @@ public class ImportControllerImpl implements ImportController {
     @Override
     @Asynchronous
     public void importBuild(Build build, CallbackTarget callback, String username) {
-        logger.log(Level.INFO, "Entering importBuild.");
+        logger.log(Level.INFO, "Importing external build {0} to tag {1}", new Object[]{build.getExternalBuildID(), build.getTagPrefix()});
 
         BuildRecordPushResultRestBuilder response = BuildRecordPushResultRest.builder();
         response.buildRecordId(build.getExternalBuildID());
@@ -98,7 +98,7 @@ public class ImportControllerImpl implements ImportController {
     @Override
     @Asynchronous
     public void untagBuild(TaggedBuild build, CallbackTarget callback) {
-        logger.log(Level.INFO, "Entering importBuild.");
+        logger.log(Level.INFO, "Untaging build {0} from tag {1}", new Object[]{build.getBrewBuildId(), build.getTagPrefix()});
 
         UntagResultRestBuilder response = UntagResultRest.builder();
         response.brewBuildId(build.getBrewBuildId());
